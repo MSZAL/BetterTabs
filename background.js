7afc52f82bb5e712532ebded383be716b2eb0ac6
Michael <szalmichael@gmail.com>
2017-10-28 09:30:01 -0500

    Fixed sorting with pinned tabs

--- background.js
+++ background.js
@@ -4,26 +4,33 @@ function domain (tab) {
 }
 
 function sort () {
+    var pinnedTabs = 0;
+        
     chrome.tabs.query( {
-	currentWindow: true
+	currentWindow: true,
     }, function (tab) {
 	var sites = [];
 	for (var i = 0; i < tab.length; i++){
-	    var d = domain(tab[i]);
-	    var inList = false;
-	    
-	    for (var l = 0; l < sites.length; l++){
-		if (sites[l] == d){
-		    inList = true;
-		}
+	    if (tab[i].pinned) {
+		pinnedTabs++;
 	    }
+	    else {
+		var d = domain(tab[i]);
+		var inList = false;
+		
+		for (var l = 0; l < sites.length; l++){
+		    if (sites[l] == d){
+			inList = true;
+		    }
+		}
 
-	    if (!inList) {
-		sites.push(domain(tab[i]));
+		if (!inList) {
+		    sites.push(domain(tab[i]));
+		}
 	    }
 	}
 
-	var count = 0;
+	var count = pinnedTabs;
 	
 	for (var k = 0; k < sites.length; k++){
 	    //alert(sites[k]);
