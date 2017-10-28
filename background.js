9d6e089b4d47f774dfbe9f9217f9d6d01f48b6d1
Michael <szalmichael@gmail.com>
2017-10-28 02:19:19 -0500

    Added background.js

---
+++ background.js
@@ -0,0 +1,31 @@
+function domain(url) {
+    //splice url at third '/'
+    //index = url.indexOf('/', 8);
+    //return url.slice(0, index);
+    var site = str.split("/");
+    return site[2];
+}
+/*
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.query({
+        currentWindow: true
+    },function(tab) {
+	
+
+	
+        var tabList = [];
+        for (var i in tabs) {
+            var i_url = domain(i.url);
+            if (!(i_url in tabList)) {
+                var count = i.index;
+                tabList.push(i_url);
+                for (var x in tabs(i.index)) {
+                    if(domain(x.url) == i_url) {
+                        chrome.tabs.move(x.id, {index: count++});
+                    }
+                }
+            }
+        }
+    }); // End of chrome.tabs.query); 
+});
+*/
