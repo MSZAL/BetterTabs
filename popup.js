8556509bbcaca7e6e6b67b18d921ddfb513a4d1a
Vladislav Yourtchenko <vyourtchenko@gmail.com>
2017-10-28 17:47:30 -0500

    cleaned

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 window.onload =  function() {
-    document.getElementById("closetabs").addEventListener("click", function() {
-	alert("hello");
+    document.getElementById("close_tabs").addEventListener("click", function() {
+	   alert("hello");
+    });
+    
+    document.getElementById("combine_windows").addEventListener("click", function() {
+	   alert("hello");
     });
 };
