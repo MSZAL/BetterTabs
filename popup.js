785935b82dcd6c770c507f9fd19254081543f60d
Michael <szalmichael@gmail.com>
2017-10-28 21:21:06 -0500

    added close tabs functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,40 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById("close_tabs");
+
+    link.addEventListener('click', function () {
+	getActiveTab(removeTabs);
+    });
+});
+function domain (tab) {
+    var x = tab.url.split("/");
+    return x[2];
+}
+
+function getActiveTab(callback) {
+    chrome.tabs.query({
+	active: true,
+	currentWindow: true
+    }, function(tab) {
+	callback(tab)
+    });
+}
+
+function removeTabs(tab){
+    chrome.tabs.query({
+	currentWindow: true
+    }, function (tabs) {
+	//console.log(tab[0].url == tabs[1].url);
+	var x = domain(tab[0]);
+
+	for (var i = 0; i < tabs.length; i++){
+	    if (x == domain(tabs[i])){
+		chrome.tabs.remove(tabs[i].id);
+	    }
+	}
+    });
+}
+/*
 window.onload =  function() {
     document.getElementById("close_tabs").addEventListener("click", function() {
         alert("hello");
@@ -7,3 +44,4 @@ window.onload =  function() {
         newWindow();
     });
 };
+*/
